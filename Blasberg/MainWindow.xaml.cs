0b3eefd17e81ee2bee97abdc54161b2f7cf63e83
haskil <haskil37@gmail.com>
2017-03-27 16:10:49 +0300

    end of parsing

--- Blasberg/MainWindow.xaml.cs
+++ Blasberg/MainWindow.xaml.cs
@@ -63,7 +63,6 @@ namespace Blasberg
         }
         #region Чтение файла с программой
         private string Path = "3.AWL";
-        private List<string> tempProgramList = new List<string>();
         private bool ReadFileDB()
         {
             if (!File.Exists(Path))
@@ -87,20 +86,29 @@ namespace Blasberg
                         start = 1;
                 }
                 ParseDB(tempDB);
+
                 start = 0;
+                List<string> tempProgramList = new List<string>();
                 while (true)
                 {
                     string temp = fs.ReadLine();
                     if (temp == null) break;
                     if (temp.Contains("FUNCTION FC") && start != 1)
                         start = 1;
-                    if (start == 1 && !temp.Contains("NOP"))
-                        tempProgramList.Add(temp);
-                    if (temp.Contains(": NOP"))
-                        tempProgramList.Add(temp);
+                    
+                    if (start == 1 && temp.Trim().Length != 0)
+                        if (!temp.Contains("NOP")
+                            && !temp.Contains("FUNCTION")
+                            && !temp.Contains("VERSION")
+                            && !temp.Contains("BEGIN")
+                            && !temp.Contains("NETWORK")
+                            && !temp.Contains("AUF")
+                            || temp.Contains(": NOP"))
+                            tempProgramList.Add(temp);
                 }
+                ParseProgramCode(tempProgramList);
+                //FillGrid();
             }
-            FillGrid();
             return true;
         }
         private void ParseDB_Bool(ref Dictionary<string, string> DB, string currentString)
@@ -131,17 +139,7 @@ namespace Blasberg
 
             var time = 0;
             if (splitCurrentString.Count() > 2)
-            {
-                var splitTime = splitCurrentString[2].Split('#');
-                var value = splitTime[1];
-                value = value.Replace(";", "");
-                value = value.Trim();
-
-                if (value.Contains("ms"))
-                    time = Convert.ToInt32(value.Replace("ms", ""));
-                else
-                    time = Convert.ToInt32(value.Replace("s", "")) * 1000;
-            }
+                time = ParseTime(splitCurrentString[2]);
 
             if (AdressCurrentString.Count() > 1)
             {
@@ -153,6 +151,21 @@ namespace Blasberg
                 DB.Add(AdressCurrentString[0].Trim(), time + "ms");
                 Stekanie.Add(AdressCurrentString[0].Trim(), time);
             }
+        }
+        private int ParseTime(string content)
+        {
+            var time = 0;
+            var splitTime = content.Split('#');
+            var value = splitTime[1];
+            value = value.Replace(";", "");
+            value = value.Trim();
+
+            if (value.Contains("ms"))
+                time = Convert.ToInt32(value.Replace("ms", ""));
+            else
+                time = Convert.ToInt32(value.Replace("s", "")) * 1000;
+
+            return time;
         }
         private void ParseDB_Int(ref Dictionary<string, string> DB, string currentString)
         {
@@ -195,85 +208,59 @@ namespace Blasberg
                     ParseDB_Int(ref DataBase, currentString);
             }
         }
-        private void FillGrid()
+        private void ParseProgramCode(List<string> content)
         {
-            var countKey = 0;
-            //var countText = 0;
-            foreach (string item in tempProgramList) // Загоняем в таблицу данные программы из файла
+            int key = 0;
+            foreach (var item in content)
             {
-                try
+                if (item.Contains("TITLE"))
                 {
-                    if (item.Contains("NETWORK") || item.Contains("TITLE") || item.Contains("END") || item.Contains("FUNCTION FC") || item.Contains("VERSION") || item.Contains("BEGIN") || item.Contains("AUF   DB"))
-                    {
-                        //var result = new ProgramData(0, item, "", "");
-                        //ProgramData.Add(result);
-                        if (StartEnd.Count != 0) //Разбитие на подпрограммы
-                        {
-                            var lastStart = StartEnd.Last();
-                            if (lastStart.Key == lastStart.Value)
-                                StartEnd[lastStart.Key] = countKey ;
-                        }
-                        //countText++;
-                    }
-                    else if (item.Trim().Length != 0)
-                    {
-                        if (StartEnd.Count != 0) //Разбитие на подпрограммы
-                        {
-                            var lastStart = StartEnd.Last();
-                            if (lastStart.Key != lastStart.Value)
-                                StartEnd.Add(countKey + 1, countKey + 1);
-                        }
-                        else
-                            StartEnd.Add(0, 0);
-
-                        var itemSplit = item.Replace(';', ' ');
-                        var stringData = itemSplit.Split(' ').ToList();
-                        stringData.RemoveAll(RemoveEmpty);
-                        countKey++;
-                        if (stringData.Count > 2)
-                        {
-                            var result = new ProgramData(countKey, stringData[0], stringData[1], stringData[2]);
-                            ProgramData.Add(result);
-                            if (stringData.Contains("FP"))
-                                FrontP.Add(countKey.ToString(), 1);
-                            if (stringData.Contains("FN"))
-                                FrontN.Add(countKey.ToString(), 0);
-                        }
-                        else if (stringData.Count == 2)
-                        {
-                            if (stringData.Contains("SPBNB"))
-                            {
-                                var result = new ProgramData(countKey, stringData[0], stringData[1], "");
-                                ProgramData.Add(result);
-                            }
-                            else if (stringData.Contains("BLD"))
-                            {
-                                var result = new ProgramData(countKey, stringData[0], stringData[1], "");
-                                ProgramData.Add(result);
-                            }
-                            else if (stringData.Contains("S5T"))
-                            {
-                                var stringTimer = stringData[1].Split('#');
-                                var result = new ProgramData(countKey, stringData[0], stringTimer[0], stringTimer[1]);
-                                ProgramData.Add(result);
-                            }
-                            else if (stringData.Contains("L"))
-                            {
-                                var result = new ProgramData(countKey, stringData[0], stringData[1], "");
-                                ProgramData.Add(result);
-                            }
-                        }
-                        else
-                        {
-                            var result = new ProgramData(countKey, stringData[0], "", "");
-                            ProgramData.Add(result);
-                        }
-                    }
+                    if (StartEnd.Count != 0) //Разбитие на подпрограммы
+                        StartEnd[StartEnd.Last().Key] = key - 1;
                 }
-                catch
+                else
                 {
+                    if (StartEnd.Count != 0) //Разбитие на подпрограммы
+                    {
+                        if (StartEnd.Last().Key != StartEnd.Last().Value)
+                            StartEnd.Add(key, key);
+                    }
+                    else
+                        StartEnd.Add(key, key);
+                    //Занесение кода программы в переменную 
+                    var itemTemp = item.Replace(';', ' ');
+                    var contentSplit = itemTemp.Split(' ').ToList();
+                    contentSplit.RemoveAll(RemoveEmpty);
+                    var firstValue = "";
+                    var secondValue = "";
+                    var thirdValue = "";
+                    if (contentSplit.Count > 2)
+                    {
+                        firstValue = contentSplit[0].Trim();
+                        secondValue = contentSplit[1].Trim();
+                        thirdValue = contentSplit[2].Trim();
+                        //Создание пременных циклов
+                        if (contentSplit.Contains("FP"))
+                            FrontP.Add(key.ToString(), 0);
+                        if (contentSplit.Contains("FN"))
+                            FrontN.Add(key.ToString(), 0);
+                    }
+                    else if (contentSplit.Count == 2)
+                    {
+                        if (contentSplit.Contains("S5T"))
+                            thirdValue = ParseTime(contentSplit[1]).ToString();
+                        firstValue = contentSplit[0].Trim();
+                        secondValue = contentSplit[1].Trim();
+                    }
+                    else
+                        firstValue = contentSplit[0].Trim();
+
+                    var result = new ProgramData(key, firstValue, secondValue, thirdValue);
+                    ProgramData.Add(result);
+                    key++;
                 }
             }
+            StartEnd[StartEnd.Last().Key] = key - 1;
         }
         private bool RemoveEmpty(String s)
         {
